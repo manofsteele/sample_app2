3c7b9fac4c7cfe21c22248a81f97951c7c9f041a
jsteele <jeremiahsteele@gmail.com>
2014-04-06 12:25:13 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,16 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+    
   end
 
   describe "Help page" do
@@ -24,7 +30,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("CS 232 Rails Development | Help")
     end
   end
 
@@ -37,8 +43,61 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("CS 232 Rails Development | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'CS 232 Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('CS 232 Contact')
     end
 
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("CS 232 Rails Development | Contact")
+    end
+
+    it "should have element <h1 class=\"page-title\">" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector("h1[class=page-title]")
+    end
+
+    it "should have element <dl>" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector("dl")
+    end
+
+    it "should have element <dt>" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector("dt")
+    end
+
+    it "should have element <dd>" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector("dd")
+    end
+
+    it "should have element <section class=\"main\">" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector("section[class=main]")
+    end
+
+    it "should have css selector h1.page-title " do
+      visit '/static_pages/contact'
+      expect(page).to have_css('h1.page-title')
+
+    end
+
+    it "should have css selector .main" do
+      visit '/static_pages/contact'
+      expect(page).to have_css('.main')
+
+    end
+
+
   end
+
 end
+
