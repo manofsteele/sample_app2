a2e418974ce92ef0f43d4232a0b2fcbf42c93e2e
jsteele <jeremiahsteele@gmail.com>
2014-04-06 13:41:04 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,100 +2,46 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+	
   describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+	before { visit root_path }
+	
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
     
   end
 
   describe "Help page" do
+	before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("CS 232 Rails Development | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("CS 232 Rails Development | About Us")
-    end
+	before { visit about_path }
+ 
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+    
   end
 
   describe "Contact page" do
+  
+    before { visit contact_path }
 
-    it "should have the content 'CS 232 Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('CS 232 Contact')
-    end
+    it { should have_content('CS 232 Contact') }
+    it { should have_title(full_title('Contact')) }
 
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("CS 232 Rails Development | Contact")
-    end
-
-    it "should have element <h1 class=\"page-title\">" do
-      visit '/static_pages/contact'
-      expect(page).to have_selector("h1[class=page-title]")
-    end
-
-    it "should have element <dl>" do
-      visit '/static_pages/contact'
-      expect(page).to have_selector("dl")
-    end
-
-    it "should have element <dt>" do
-      visit '/static_pages/contact'
-      expect(page).to have_selector("dt")
-    end
-
-    it "should have element <dd>" do
-      visit '/static_pages/contact'
-      expect(page).to have_selector("dd")
-    end
-
-    it "should have element <section class=\"main\">" do
-      visit '/static_pages/contact'
-      expect(page).to have_selector("section[class=main]")
-    end
-
-    it "should have css selector h1.page-title " do
-      visit '/static_pages/contact'
-      expect(page).to have_css('h1.page-title')
-
-    end
-
-    it "should have css selector .main" do
-      visit '/static_pages/contact'
-      expect(page).to have_css('.main')
-
-    end
-
+    it { should have_selector("h1[class=page-title]") }
+    it { should have_selector("dl") }
+    it { should have_selector("dt") }
+    it { should have_selector("dd") }
+    it { should have_selector("section[class=main]") }
+    it { should have_css('h1.page-title') }
+    it { should have_css('.main') }
 
   end
 
